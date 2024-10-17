bca588b51ddfcb1e7ef596149b4386572f003d84
camilajerozolimski <camilajerozolimski@gmail.com>
2024-10-17 21:39:35 +0000

    Entrega examen parcial 1

---
+++ parcial1/puntoe.sh
@@ -0,0 +1,4 @@
+grep "MemTotal" /proc/meminfo > Filtro_Basico.txt
+dmidecode -t chassis | grep "Manufacturer" >> Filtro_Basico.txt
+
+
---
+++ parcial1/puntof.sh
@@ -0,0 +1,8 @@
+cat <<EOL > ~/parcial1/Filtro_Avanzado.txt
+
+curl -s ifconfig.me
+/etc/passwd
+/etc/shadow
+EOL
+
+
